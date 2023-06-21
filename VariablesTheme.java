bc8f0e0fb1c728401ca5405d38b0d673a2aa3796
Anton Gridin <titokila17@gmail.com>
2023-06-21 19:49:13 +0300

    Исправление таюуляции

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -1,5 +1,5 @@
 public class VariablesTheme {
-
+    
     public static void main(String[] args) {
         System.out.println("1. Вывод характеристик компьютера\n");
         byte cpuGeneration = 13;
