bb8cc8fa37ff74e82dd1f88a9b0f6f07bda18576
Anton Gridin <titokila17@gmail.com>
2023-07-13 22:09:04 +0300

    Загрузка Lesson_2 ДЗ_1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+
+    String gender = "Men";
+    String name = "Anton";
+    int age = 31;
+    double weight = 93.5;
+
+    void showInfo() {
+        System.out.println("I'm a " + gender + "\nMy name is " name + "\nMy age is " + age + 
+                "\nmy weight is " + weight);
+    }
+
+    void wakeUp() {
+        System.out.println(name + " wakes up");
+    }
+
+    void goWork() {
+        System.out.println(name + " is going to work");
+    }
+
+    void work() {
+        System.out.println(name + " is working");
+    }
+
+    void goHome() {
+        System.out.println(name + " is going home");
+    }
+
+    void sleep() {
+        System.out.println(name + " falls asleep");
+    }
+}
