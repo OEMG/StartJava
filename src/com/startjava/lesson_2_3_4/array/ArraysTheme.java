9ddfeb89fe81e70e0ef65028da5b060f155f90b8
Anton Gridin <titokila17@gmail.com>
2023-09-14 22:10:55 +0300

    Исправлены замечания ArraysTheme ver. 2

--- src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ArraysTheme {
     public static void main(String[] args) {
         reverseArray();
-        multiplicationNumbers();
+        multiplyNumbers();
         removeFromArray();
         showAlphabetLadder();
         fillWithUniqueNumbers();
@@ -13,65 +13,63 @@ public class ArraysTheme {
 
     public static void reverseArray() {
         System.out.println("1. Реверс значений массива");
-        int[] unsorted = {1, 7, 4, 5, 2, 6, 3};
-        System.out.println("   До реверса: " + Arrays.toString(unsorted));
-        int len = unsorted.length;
+        int[] numbers = {1, 7, 4, 5, 2, 6, 3};
+        System.out.println("   До реверса: " + Arrays.toString(numbers));
+        int len = numbers.length;
         for (int i = 0; i < len / 2; i++) {
-            int backIndex = len - i - 1;
-            int temp = unsorted[i];
-            unsorted[i] = unsorted[backIndex];
-            unsorted[backIndex] = temp;
+            int backIndex = --len;
+            int temp = numbers[i];
+            numbers[i] = numbers[backIndex];
+            numbers[backIndex] = temp;
         }
-        System.out.println("После реверса: " + Arrays.toString(unsorted));
+        System.out.println("После реверса: " + Arrays.toString(numbers));
     }
 
-    public static void multiplicationNumbers() {
+    public static void multiplyNumbers() {
         System.out.println("\n2. Произведение элементов массива");
-        int[] digits = new int[10];
-        int len = digits.length;
+        int[] multipliers = new int[10];
+        int len = multipliers.length;
         for (int i = 0; i < len; i++) {
-            digits[i] = i;
+            multipliers[i] = i;
         }
         int result = 1;
         for (int i = 1; i < len - 1; i++) {
-            result *= digits[i];
-            System.out.print(digits[i]);
+            result *= multipliers[i];
+            System.out.print(multipliers[i]);
             System.out.print((len - 2 > i) ? (" * ") : (" = " + result));
         }
     }
 
     public static void removeFromArray() {
         System.out.println("\n\n3. Удаление элементов массива");
-        double[] random = new double[15];
-        int len = random.length;
+        double[] randomNumbers = new double[15];
+        int len = randomNumbers.length;
         for (int i = 0; i < len; i++) {
-            random[i] = Math.random();
+            randomNumbers[i] = Math.random();
         }
-        printArray(random);
-        System.out.println();
-        int countZero = 0;
-        double midElement = random[len / 2];
+        printArray(randomNumbers);
+        int countZeros = 0;
+        double middleElement = randomNumbers[len / 2];
         for (int i = 0; i < len; i++) {
-            if (random[i] > midElement) {
-                random[i] = 0;
-                countZero++;
+            if (randomNumbers[i] > middleElement) {
+                randomNumbers[i] = 0;
+                countZeros++;
             }
         }
-        printArray(random);
-        System.out.println("\nКоличество нулей " + countZero);
+        printArray(randomNumbers);
+        System.out.println("Количество нулей " + countZeros);
     }
 
     public static void showAlphabetLadder() {
         System.out.println("\n4. Вывод алфавита лесенкой");
-        char[] letters = new char[26];
-        int len = letters.length;
-        int i = 0;
-        for (char ch = 'A'; ch <= 'Z'; ch++) {
-            letters[i++] = ch;
+        char[] alphabet = new char[26];
+        int len = alphabet.length;
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = (char) ('A' + i);
         }
-        for (i = len - 1; i >= 0; i--) {
+        for (int i = len - 1; i >= 0; i--) {
             for (int j = len - 1; j >= i; j--) {
-                System.out.print(letters[j]);
+                System.out.print(alphabet[j]);
             }
             System.out.println();
         }
@@ -79,20 +77,20 @@ public class ArraysTheme {
 
     public static void fillWithUniqueNumbers() {
         System.out.println("\n5. Заполнение массива уникальными числами");
-        int[] unique = new int[30];
-        int len = unique.length;
+        int[] uniqueNumbers = new int[30];
+        int len = uniqueNumbers.length;
         //Заполняем массив и сразу проверяем на уникальность
         for (int i = 0; i < len; i++) {
             int temp = (int) (Math.random() * 40 + 60);
             boolean isDuplicate = false;
             for (int j = 0; j < i; j++) {
-                if (unique[j] == temp) {
+                if (uniqueNumbers[j] == temp) {
                     isDuplicate = true;
                     break;
                 }
             }
             if (!isDuplicate) {
-                unique[i] = temp;
+                uniqueNumbers[i] = temp;
             } else {
                 i--;
             }
@@ -100,16 +98,16 @@ public class ArraysTheme {
         //Сортировка пузырьком
         for (int i = 0; i < len - 1; i++) {
             for (int j = 0; j < len - i - 1; j++) {
-                if (unique[j] > unique[j + 1]) {
-                    int temp = unique[j];
-                    unique[j] = unique[j + 1];
-                    unique[j + 1] = temp;
+                if (uniqueNumbers[j] > uniqueNumbers[j + 1]) {
+                    int temp = uniqueNumbers[j];
+                    uniqueNumbers[j] = uniqueNumbers[j + 1];
+                    uniqueNumbers[j + 1] = temp;
                 }
             }
         }
         //Вывод по 10 значений в строке
         for (int i = 0; i < len; i++) {
-            System.out.print(unique[i] + " ");
+            System.out.print(uniqueNumbers[i] + " ");
             if ((i + 1) % 10 == 0) {
                 System.out.println();
             }
@@ -123,7 +121,6 @@ public class ArraysTheme {
             }
             System.out.printf("%.3f ", array[i]);
         }
+        System.out.println();
     }
-
-
 }
