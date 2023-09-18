17b3cea51e975ff87bda8158906d9d3226a09809
Anton Gridin <titokila17@gmail.com>
2023-09-19 00:01:03 +0300

    Обновление калькулятора Урок 4 ver.2

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,17 +2,11 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    private String[] expression;
-
-    public void setExpression(String expression) {
-        this.expression = expression.split(" ");
-    }
-
-    public double calculate() {
-        int num1 = Integer.parseInt(expression[0]);
-        char sign = expression[1].charAt(0);
-        int num2 = Integer.parseInt(expression[2]);
-        System.out.print(num1 + " " + sign + " " + num2 + " = ");
+    public double calculate(String expression) {
+        String[] components = expression.split(" ");
+        int num1 = Integer.parseInt(components[0]);
+        char sign = components[1].charAt(0);
+        int num2 = Integer.parseInt(components[2]);
         return switch (sign) {
             case '+' -> num1 + num2;
             case '-' -> num1 - num2;
@@ -22,7 +16,7 @@ public class Calculator {
             case '^' -> Math.pow(num1, num2);
             default -> {
                 System.out.println("Ошибка: знак " + sign + " не поддерживается");
-                yield 0;
+                yield Double.MIN_VALUE;
             }
         };
     }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -8,15 +8,17 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
-        DecimalFormat dF = new DecimalFormat("#.###");
+        DecimalFormat df = new DecimalFormat("#.###");
         String answer = "yes";
         do {
             if ("yes".equals(answer)) {
                 System.out.print("\nВведите математическое выражение: ");
-                calc.setExpression(scan.nextLine());
-                System.out.println(dF.format(calc.calculate()));
+                String expression = scan.nextLine();
+                if (calc.calculate(expression) != Double.MIN_VALUE) {
+                    System.out.print(expression + " = " + df.format(calc.calculate(expression)));
+                }
             }
-            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+            System.out.print("\nХотите продолжить вычисления? [yes/no]: ");
             answer = scan.next();
             scan.nextLine();
         } while (!"no".equals(answer));
