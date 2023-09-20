538fea7ff265f4b581132db446b9d573bb9782c0
Anton Gridin <titokila17@gmail.com>
2023-09-20 21:14:51 +0300

    Обновление калькулятора Урок 4 ver.3

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -8,20 +8,24 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("#.###");
         String answer = "yes";
         do {
             if ("yes".equals(answer)) {
                 System.out.print("\nВведите математическое выражение: ");
                 String expression = scan.nextLine();
-                if (calc.calculate(expression) != Double.MIN_VALUE) {
-                    System.out.print(expression + " = " + df.format(calc.calculate(expression)));
-                }
+                double result = calc.calculate(expression);
+                print(expression, result);
             }
             System.out.print("\nХотите продолжить вычисления? [yes/no]: ");
-            answer = scan.next();
-            scan.nextLine();
+            answer = scan.nextLine();
         } while (!"no".equals(answer));
         scan.close();
+    }
+
+    private static void print(String expression, double result) {
+        DecimalFormat df = new DecimalFormat("#.###");
+        if (result != Double.MIN_VALUE) {
+            System.out.print(expression + " = " + df.format(result));
+        }
     }
 }
