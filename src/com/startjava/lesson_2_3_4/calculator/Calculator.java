3df758c263f2bd3fe479210bb2e97f2f32ce8120
Anton Gridin <titokila17@gmail.com>
2023-09-22 17:44:52 +0300

    Обновление калькулятора Урок 4 со*

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,22 +2,50 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public double calculate(String expression) {
+    public static double calculate(String expression) {
         String[] components = expression.split(" ");
-        int num1 = Integer.parseInt(components[0]);
-        char sign = components[1].charAt(0);
-        int num2 = Integer.parseInt(components[2]);
+        if (components.length != 3) {
+            throw new RuntimeException("количество элементов должно быть 3");
+        }
+        int num1, num2;
+        try {
+            num1 = Integer.parseInt(components[0]);
+            num2 = Integer.parseInt(components[2]);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("введенные числа не являются целыми");
+        }
+        if (num1 < 0 || num2 < 0) {
+            throw new RuntimeException("введенные числа должны быть положительными");
+        }
+        char sign;
+        try {
+            sign = components[1].charAt(0);
+        } catch (RuntimeException e) {
+            //кидает если ввести 5  5 (число, два пробела и опять число)
+            throw new RuntimeException("необходимо указать знак математической операции");
+        }
         return switch (sign) {
             case '+' -> num1 + num2;
             case '-' -> num1 - num2;
             case '*' -> num1 * num2;
-            case '/' -> (double) num1 / num2;
-            case '%' -> num1 % num2;
-            case '^' -> Math.pow(num1, num2);
-            default -> {
-                System.out.println("Ошибка: знак " + sign + " не поддерживается");
-                yield Double.MIN_VALUE;
+            case '/' -> {
+                double result = (double) num1 / num2;
+                checkingZero(result);
+                yield result;
             }
+            case '%' -> {
+                double result = num1 % num2;
+                checkingZero(result);
+                yield result;
+            }
+            case '^' -> Math.pow(num1, num2);
+            default -> throw new RuntimeException("знак " + sign + " не поддерживается");
         };
+    }
+
+    private static void checkingZero(double result) {
+        if (Double.isInfinite(result)) {
+            throw new RuntimeException("нельзя делить на 0");
+        }
     }
 }
