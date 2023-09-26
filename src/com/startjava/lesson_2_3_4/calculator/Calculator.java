0b7dad0550b3e4acf06811abeba46d516f1ebe09
Anton Gridin <titokila17@gmail.com>
2023-09-26 21:49:25 +0300

    Вынес проверки в отдельные методы Calculator

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -7,7 +7,24 @@ public class Calculator {
         if (components.length != 3) {
             throw new RuntimeException("количество элементов должно быть 3");
         }
-        int num1, num2;
+        int[] numbers = checkNumbers(components);
+        int num1 = numbers[0];
+        int num2 = numbers[1];
+        char sign = checkingSign(components);
+        return switch (sign) {
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case '*' -> num1 * num2;
+            case '/' -> (double) num1 / num2;
+            case '%' -> num1 % num2;
+            case '^' -> Math.pow(num1, num2);
+            default -> throw new RuntimeException("знак " + sign + " не поддерживается");
+        };
+    }
+
+    private static int[] checkNumbers(String[] components) {
+        int num1;
+        int num2;
         try {
             num1 = Integer.parseInt(components[0]);
             num2 = Integer.parseInt(components[2]);
@@ -17,33 +34,19 @@ public class Calculator {
         if (num1 < 0 || num2 < 0) {
             throw new RuntimeException("введенные числа должны быть положительными");
         }
+        if (num2 == 0) {
+            throw new RuntimeException("нельзя делить на 0");
+        }
+        return new int[]{num1, num2};
+    }
+
+    private static char checkingSign(String[] components) {
         char sign;
         try {
             sign = components[1].charAt(0);
         } catch (RuntimeException e) {
-            //кидает если ввести 5  5 (число, два пробела и опять число)
             throw new RuntimeException("необходимо указать знак математической операции");
         }
-        return switch (sign) {
-            case '+' -> num1 + num2;
-            case '-' -> num1 - num2;
-            case '*' -> num1 * num2;
-            case '/' -> {
-                checkingZero(num2);
-                yield (double) num1 / num2;
-            }
-            case '%' -> {
-                checkingZero(num2);
-                yield num1 % num2;
-            }
-            case '^' -> Math.pow(num1, num2);
-            default -> throw new RuntimeException("знак " + sign + " не поддерживается");
-        };
-    }
-
-    private static void checkingZero(int number) {
-        if (number == 0) {
-            throw new RuntimeException("нельзя делить на 0");
-        }
+        return sign;
     }
 }
